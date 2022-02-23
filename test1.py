345693704d4c8262919620d56e61c76b00983d6e
2693176767 <99626471+2693176767@users.noreply.github.com>
2022-02-23 17:26:16 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,19 @@
 from sklearn import tree #导入需要的模块
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
+from main import main
+from utils import AMLConfigurationException
+import pytest
 wine=load_wine()
 Xtrain,Xtest,Ytrain,Ytest=train_test_split(wine.data,wine.target,test_size=0.3)#30%作为测试集，70%作为训练集
 clf=tree.DecisionTreeClassifier(criterion="entropy")
 clf=clf.fit(Xtrain,Ytrain)
 score=clf.score(Xtest,Ytest)#返回预测准确度accuracy
 print(score)
-print(github.event.inputs.mode )
+
+def test_main_no_input():
+    """
+    Unit test to check the main function with no inputs
+    """
+    with pytest.raises(AMLConfigurationException):
+        assert main()
