b364e0779341689d0d5023a221a8aa2f88538719
2693176767 <99626471+2693176767@users.noreply.github.com>
2022-02-23 11:34:12 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,9 @@
-
+from sklearn import tree #导入需要的模块
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+wine=load_wine()
+Xtrain,Xtest,Ytrain,Ytest=train_test_split(wine.data,wine.target,test_size=0.3)#30%作为测试集，70%作为训练集
+clf=tree.DecisionTreeClassifier(criterion="entropy")
+clf=clf.fit(Xtrain,Ytrain)
+score=clf.score(Xtest,Ytest)#返回预测准确度accuracy
+print(score)
