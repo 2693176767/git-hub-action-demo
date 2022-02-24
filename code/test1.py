4f613462d5a8e3b61f1bb364dc0de095395ddcc7
2693176767 <99626471+2693176767@users.noreply.github.com>
2022-02-24 16:25:49 +0800

    Update test1.py

--- code/test1.py
+++ code/test1.py
@@ -1,34 +1,34 @@
-pip install sklearn
-from sklearn import tree #导入需要的模块
-from sklearn.datasets import load_wine
-from sklearn.model_selection import train_test_split
+#pip install sklearn
+#from sklearn import tree #导入需要的模块
+#from sklearn.datasets import load_wine
+#from sklearn.model_selection import train_test_split
 #import pytest
 import os
-wine=load_wine()
-Xtrain,Xtest,Ytrain,Ytest=train_test_split(wine.data,wine.target,test_size=0.3)#30%作为测试集，70%作为训练集
-clf=tree.DecisionTreeClassifier(criterion="entropy")
-clf=clf.fit(Xtrain,Ytrain)
-score=clf.score(Xtest,Ytest)#返回预测准确度accuracy
-print(score)
-def main():
+#wine=load_wine()
+#Xtrain,Xtest,Ytrain,Ytest=train_test_split(wine.data,wine.target,test_size=0.3)#30%作为测试集，70%作为训练集
+#clf=tree.DecisionTreeClassifier(criterion="entropy")
+#clf=clf.fit(Xtrain,Ytrain)
+#score=clf.score(Xtest,Ytest)#返回预测准确度accuracy
+#print(score)
+#def main():
     #Loading input values
-    print("::debug::Loading input values")
-    development = os.environ.get("INPUT_DEVELOPMENT", default=None)
-    mode = os.environ.get("INPUT_MODE", default=None)
-    print(development)
-    print(mode)
+print("::debug::Loading input values")
+development = os.environ.get("INPUT_DEVELOPMENT", default=None)
+mode = os.environ.get("INPUT_MODE", default=None)
+print(development)
+print(mode)
     
     # Casting input values
-    print("::debug::Casting input values")
-    try:
-        mode = int(mode)
-    except TypeError as exception:
-        print(f"::debug::Could not cast mode to int: {exception}")
-        mode= None
-    except ValueError as exception:
-        print(f"::debug::Could not cast mode to int: {exception}")
-        mode = None
+    #print("::debug::Casting input values")
+    #try:
+        #mode = int(mode)
+    #except TypeError as exception:
+        #print(f"::debug::Could not cast mode to int: {exception}")
+        #mode= None
+    #except ValueError as exception:
+        #print(f"::debug::Could not cast mode to int: {exception}")
+        #mode = None
     
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+    #main()
 
