6658c1a46c62b9134ef7454edd268d6a1483322e
2693176767 <99626471+2693176767@users.noreply.github.com>
2022-02-24 09:11:08 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,6 @@
 from sklearn import tree #导入需要的模块
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from main import main
-from utils import AMLConfigurationException
 import pytest
 wine=load_wine()
 Xtrain,Xtest,Ytrain,Ytest=train_test_split(wine.data,wine.target,test_size=0.3)#30%作为测试集，70%作为训练集
