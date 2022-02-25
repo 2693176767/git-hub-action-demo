8e36be17a7f7dfb8a4994fd3cde8e31781c3087e
2693176767 <99626471+2693176767@users.noreply.github.com>
2022-02-25 10:01:56 +0800

    Update entrypoint.sh

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-python -m pip install sklearn
-python /code/test1.py
+python -m pip install sklearn #在doker环境下下载需要用到的机器学习库sklearn
+python /code/test1.py #打开code文件夹，在docker环境下运行test1.py文件
